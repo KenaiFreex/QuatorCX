5ef98944ec5ee31e9be8ad13382c6cbb9fdad72f
kenaiFreex <virtualfreex@gmail.com>
2019-02-15 14:30:19 -0600

    Add an option to execute a python script on index initialization

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,15 @@ const router = require('express').Router();
 router.get ('/',(req,res) => {
 
     res.send('Bienvenido al Index :)');
-
+    
+    
+   // var spawn = require("child_process").spawn;
+   // var process = spawn('python',["./hello.py", 'Hello world'] );
+  
+   // process.stdout.on('data',(data) =>{
+   //     res.send(data.toString());
+   //  })
+    
 });
 
 module.exports = router;
