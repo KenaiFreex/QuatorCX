7f6ddacd9a2730b29de70be8990fbced70e33c1e
kenaiFreex <virtualfreex@gmail.com>
2019-02-21 04:08:30 -0600

    Adding Basic Routes to App

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,46 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-router.get ('/',(req,res) => {
+router.get('/', (req, res) => {
+  res.render('index');
+});
 
-    res.send('Bienvenido al Index :)');
-    
-    
-   // var spawn = require("child_process").spawn;
-   // var process = spawn('python',["./hello.py", 'Hello world'] );
-  
-   // process.stdout.on('data',(data) =>{
-   //     res.send(data.toString());
-   //  })
-    
+router.get('/about', (req, res) => {
+  res.render('about');
 });
 
 module.exports = router;
+
+
+
+/*const router = require('express').Router();
+
+router.get ('/',(req,res) => {
+
+ res.render('index');
+    
+//   var spawn = require("child_process").spawn;
+//    var process = spawn('python',["src/hello.py", 'Kenai'] );
+  
+//    process.stdout.on('data',(data) =>{
+//        res.send(data.toString());
+//  })
+    
+
+});
+
+router.get ('/about',(req,res) => {
+
+    res.render('about');
+       
+   //   var spawn = require("child_process").spawn;
+   //    var process = spawn('python',["src/hello.py", 'Kenai'] );
+     
+   //    process.stdout.on('data',(data) =>{
+   //        res.send(data.toString());
+   //  })
+       
+   
+   });
+
+module.exports = router; */
